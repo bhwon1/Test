b2c3ff1151522e52a22b9aa85e3a382175678616
bhwon1 <blm2094@naver.com>
2023-01-30 16:32:25 +0900

    day5 quiz 오류수정

--- class_quiz/pages/05-02/[page]/index.js
+++ class_quiz/pages/05-02/[page]/index.js
@@ -22,7 +22,7 @@ export default function RoutedPage(){
 
     const {data} = useQuery(FETCH_PRODUCT,{
       variables : {
-        productId : String(router.query._id)
+        productId : router.query.page
       }
     })
 
@@ -32,7 +32,6 @@ export default function RoutedPage(){
 
   return(
     <>
-    <div>{router.query.seller}페이지로 이동되었습니다.</div>
     <div>판매자 : {data ? data.fetchProduct.seller: "loading..."}</div> 
     <div>상품명 :  {data && data.fetchProduct.name}</div> 
     <div>상품내용 : {data && data.fetchProduct.detail}</div> 
