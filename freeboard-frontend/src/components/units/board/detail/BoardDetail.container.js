52e681cf6c17153f861c88007e8f087cb01f6071
bhwon1 <blm2094@naver.com>
2023-02-03 21:39:33 +0900

    day8 pf 만들기 목록 삭제 까지 완성

--- freeboard-frontend/src/components/units/board/detail/BoardDetail.container.js
+++ freeboard-frontend/src/components/units/board/detail/BoardDetail.container.js
@@ -1,12 +1,20 @@
 import BoardWriterUI from "./BoardDetail.presenter";
-import { useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import { useRouter } from 'next/router';
 import { FETCH_BOARD } from "./BoardDetail.queries";
+
+const DELETE_BOARDS = gql`
+  mutation deleteBoard($boardId : ID!){
+    deleteBoard(boardId: $boardId)
+  }
+`
 
 
 export default function BoardWriter (){
 
   const router = useRouter()
+
+  const [deleteBoard] = useMutation(DELETE_BOARDS)
 
   const { data } = useQuery(FETCH_BOARD, {
     variables: { boardId: router.query.boardId }, // 가지고 오는 위치
@@ -17,11 +25,22 @@ export default function BoardWriter (){
     router.push(`/boards/[]/edit`)
   }
 
+  const onClickDeleteList = (e)=>{
+    deleteBoard({
+      variables : {
+        boardId : String(e.target.id)
+      }
+    })
+    alert("내용이 삭제 되었습니다.")
+    router.push(`http://localhost:3000/boards/[]/edit`)
+  }
+
   return(
     <>
       <BoardWriterUI
         data={data}
         onClickMoveList={onClickMoveList}
+        onClickDeleteList={onClickDeleteList}
       />
     </>
   )
