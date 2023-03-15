af2cc7314256bc3dae4cd75083fa35db301ffaf7
bhwon1 <blm2094@naver.com>
2023-03-15 17:06:29 +0900

    day15 class_qiuz

---
+++ class_quiz/pages/15/index.tsx
@@ -0,0 +1,55 @@
+import { gql, useQuery } from "@apollo/client";
+import InfiniteScroll from "react-infinite-scroller";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function InfiniteScrollPage() {
+  const { data, fetchMore } = useQuery(FETCH_BOARDS);
+
+  const onLoadMore = () => {
+    if (data === undefined) return;
+
+    void fetchMore({
+      variables: { page: Math.ceil(data?.fetchBoards.length / 10) + 1 },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult.fetchBoards) {
+          return {
+            fetchBoards: [...prev.fetchBoards],
+          };
+        }
+        return {
+          fetchBoards: [...prev.fetchBoards, ...fetchMoreResult.fetchBoards],
+        };
+      },
+    });
+  };
+
+  return (
+    <>
+      <div style={{ overflow: "auto", height: "300px" }}>
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={onLoadMore}
+          hasMore={true}
+          useWindow={false}
+        >
+          {data?.fetchBoards.map((el) => (
+            <div key={el._id}>
+              <span style={{ margin: "10px" }}>{el.contents}</span>
+              <span style={{ margin: "10px" }}>{el.title}</span>
+            </div>
+          )) ?? <div></div>}
+        </InfiniteScroll>
+      </div>
+    </>
+  );
+}
